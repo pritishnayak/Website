6c4270b584606f912668d4dbd5fd4e198eebc4ba
pritish <pritishperfect@gmail.com>
2017-06-22 20:07:16 +0000

    Added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AlertModule} from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    AlertModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
